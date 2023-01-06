ab9f2b6437f16966604290953f29d7aedb95c565
RedSummit1 <95606327+RedSummit1@users.noreply.github.com>
2023-01-06 01:50:37 -0500

    Add files via upload

---
+++ Example_11/ex11.js
@@ -0,0 +1,37 @@
+let college = 'York College';
+const myName = 'Jane Doe';
+
+console.log(college);
+console.log(myName);
+
+const favoriteSubject = 'Math';
+//Console.log the text "My name is: " follows by myName variable.
+console.log("My name is:", myName, ", I go to ", college);
+console.log("My name is:" + myName);
+//Console.log the text "My college is: " follows by college variable.
+console.log("My college is: ", college);
+//Reassign the value stored in favoriteSubject to the string "Coding", is it possible?
+    //! No you cannot, it is constant
+//can you console.log(number1) here?
+    //! No you cannot, cannot access number before initialization 
+//Declare a new variable named 'number1' and give it the value of 10.
+
+let number1 = 10;
+//Declare a new variable named 'number2' and give it the value of 15.
+let number2 = 15;
+
+//console.log the summation of number1 and number2 in the following format
+console.log("The sume is: ",number1 + number2);
+//Expected output: The sum is 25
+//console.log the product of number1 and number2 in the following format
+console.log("The product is ", number1 * number2);
+//Expected output: The product is 150
+
+//Declare a new variable named 'check' and give it the value of true (Boolean).
+let value = true;
+//console.log check and see the value, try also with false.
+console.log(value);
+value = false;
+console.log(value);
+document.getElementById('demo') = "The sum is " +(number1 + number2);
+
