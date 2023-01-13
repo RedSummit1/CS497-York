f6ca90eeaaffcd84fb720750e0c557da459dcc85
RedSummit1 <95606327+RedSummit1@users.noreply.github.com>
2023-01-13 03:40:33 -0500

    Add files via upload

--- Example_20/ex20.js
+++ Example_20/ex20.js
@@ -13,66 +13,82 @@ let student = {
         zip: "12345",
         state: "NY"
     },
-    courses: ["CS497", "CS291", "CS341", "CS172"];
-    getAddress: function (){
-            return this.address.number +" "+ this.address.street +" "+ this.address.city +" "+ this.address.zip +" "+ this.address.state;
+    courses:["CS497", "CS291", "CS341", "CS172"],
+    
+    getAddress:function(){
+        return this.address.number +" "+ this.address.street +" "+ this.address.city +" "+ this.address.zip +" "+ this.address.state;
     }
+    
 }
+
+
 
 document.getElementById('txtName').innerHTML = student.name;
 document.getElementById("txtID").innerHTML = student.id;
-document.getElementById("txtCourses").innerHTML = "courses";
+document.getElementById("txtCourses").innerHTML = student.courses;
 document.getElementById("txtAge").innerHTML = student.age;
-document.getElementById("txtAddress").innerHTML = student.address;
+document.getElementById("txtAddress").innerHTML = student.getAddress();
 
 let empList =[
 {
-    "fname": "Sharad",
-    "lname": "Volz"
+    fname: "Sharad",
+    lname: "Volz"
 },
 {
-    "fname": "Constance",
-    "lname": "Dech"
+    fname: "Constance",
+    lname: "Dech"
 },
 {
-    "fname": "Myra",
-    "lname": "Comeau"
+    fname: "Myra",
+    lname: "Comeau"
 },
 {
-    "fname": "Noriess",
-    "lname": "Dabbs"
+    fname: "Noriess",
+    lname: "Dabbs"
 },
 {
-    "fname": "Esperanza",
-    "lname": "Shrestha"
+    fname: "Esperanza",
+    lname: "Shrestha"
 },
 {
-    "fname": "Eugene",
-    "lname": "Barron"
+    fname: "Eugene",
+    lname: "Barron"
 },
 {
-    "fname": "Petr",
-    "lname": "Ravenelle"
+    fname: "Petr",
+    lname: "Ravenelle"
 },
 {
-    "fname": "Cristina",
-    "lname": "Terry"
+    fname: "Cristina",
+    lname: "Terry"
 },
 {
-    "fname": "Victoria",
-    "lname": "Aresti"
+    fname: "Victoria",
+    lname: "Aresti"
 },
 {
-    "fname": "Byron",
-    "lname": "Yarber"
+    fname: "Byron",
+    lname: "Yarber"
 },
 ];
 
+// let strInnerHTML = "";
+// for(let i =0; i <empList.length; i++){
+//     strInnerHTML += strInnerHTML + `
+//             <tr>
+//                 <td>  ${emplist[i].fname}</td>
+//                 <td>  ${emplist[i].lname}</td>
+//             <tr>`;
+// }
 let strInnerHTML = "";
-for(let i =0; i <10; i++){
-    strInnerHTML = strInnerHTML + "<tr><td>Row" + i + "<tr><td>Row" + i + "</td></tr>";
-}
-document.getElementById.('empData').innerHTML = strInnerHTML;
+empList.map((obj) =>{
+    document.getElementById('empData').innerHTML +=`
+    <tr>
+        <td>${obj.fname}</td>
+        <td>${obj.lname}</td>
+    </tr>`
+});     
+//document.getElementById('empData').innerHTML = strInnerHTML;
 
 
 
